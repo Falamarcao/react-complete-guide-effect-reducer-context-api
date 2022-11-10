80a4473fe7468e2c796c0bd95a88e6985ff8f587
falamarcao <falamarcao@gmail.com>
2022-11-10 17:43:14 -0300

    111. Using the useEffect() Hook

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // By default, effects run after every completed render,
+    // but you can choose to fire them only when certain values have changed.
+    const isLoggedInLocalStorage = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedInLocalStorage === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "true");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "false");
     setIsLoggedIn(false);
   };
 
